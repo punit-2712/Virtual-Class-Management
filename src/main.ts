186ec3fd22b413ce7b8de4d8a9e9d22b2c5fa203
Punit Vachhani <40193957+punit-2712@users.noreply.github.com>
2024-09-21 12:28:16 +0530

    commit for assignment

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,59 @@
+//Interface for student 
 
-let message:string="hello world";
-console.log(message);
+interface Student{
+    id:string;
+    name:string;
+    classes:string[] //there might be case he is enrolled in multiple classes
+}
+
+
+interface Assignment{
+    name:string;
+    dueDate:string;
+    submissions: { [studentId: string]: 'Submitted' | 'Pending' };
+}
+
+//Interface for classroom 
+interface Classroom{
+    name:string;
+    schedule:{startTime: string; endTime: string; date:string};
+    students:string[];
+    assignments:{[key:string]:Assignment};
+}
+
+let classrooms:{[key:string]:Classroom}={};
+let students:{[key:string]:Student}={};
+
+function createClassroom(className:string, schedule:{startTime: string; endTime: string; date:string}):void{
+    if(!classrooms[className]){
+        classrooms[className]={name:className,schedule,students:[],assignments:{}};
+        console.log(`Classroom '${className}' created with schedule.`);
+    }else {
+    console.log(`Classroom '${className}' already exists.`);
+  }
+}
+
+//add student
+
+function add_student(studentId:string,studentName:string, className:string):void{
+    if(!classrooms[className]){
+        console.log(`Classroom '${className}' does not exist.`);
+        return;
+    } 
+    if(!students[studentId]){
+        students[studentId]={id:studentId, name:studentName, classes:[]};
+    }
+    classrooms[className].students.push(studentId);
+    students[studentId].classes.push(className);
+}
+
+function add_assignment(className:string, assignmentTitle:string,dueDate:string):void{
+    if(!classrooms[className]){
+        console.log(`Classroom '${className}' does not exist.`);
+        return;
+    }
+    classrooms[className].assignments[assignmentTitle]={ name: assignmentTitle, dueDate, submissions: {} };
+    classrooms[className].students.forEach(studentId=>{
+         classrooms[className].assignments[assignmentTitle].submissions[studentId] = 'Pending';
+    })
+}
